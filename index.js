f5c79086c4132101c6f06fa82e5249a3f410dfa4
Shuvro882 <sahashuvro74@gmail.com>
2026-02-04 13:52:09 +0600

    plateShare servers first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(cors());
+app.use(express.json())
+
+
+
+
+const uri = "mongodb+srv://plate-share:QQRnlL6irEwPcb8U@db-first-server.9dfabil.mongodb.net/?appName=Db-first-server";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    
+    const db = client.db('food_db')
+    const foodCollection = db.collection('food')
+    
+    //find
+    //findOne
+
+    app.get('/food', async(req,res) => {
+      
+      const result = await
+      foodCollection.find().toArray();
+     
+
+      res.send(result)
+    })
+    
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/',(req, res) =>{
+    res.send('Plate share server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Smart server is running on port:${port}`)
+})
